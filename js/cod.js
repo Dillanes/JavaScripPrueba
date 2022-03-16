a5ecc8a7d50628e9dd21a81184e495e130cc27a2
Israel Dillanes <israeldillanes2@gmail.com>
2022-03-15 19:34:56 -0600

    Practica formulario alumno

--- js/cod.js
+++ js/cod.js
@@ -1,18 +1,44 @@
-// const boton = document.querySelector(".oneClick");
-const select = document.querySelector(".selector");
-const intp = document.querySelector(".txt");
+let nombre = document.querySelector("#nombre");
+let materia = document.querySelector("#materia");
+let correo = document.querySelector("#gmail");
+let btn = document.querySelector("#btn-enviar");
+let mensaje = document.querySelector(".mensaje");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let error = validar();
 
-intp.addEventListener("keypress", (e) => {
-  console.log(e);
+  if (error[0]) {
+    mensaje.classList.remove("colorV");
+    mensaje.innerHTML = error[1];
+    mensaje.classList.add("colorR");
+  } else {
+    mensaje.innerHTML = "Formulario enviado";
+    mensaje.classList.add("colorV");
+    mensaje.classList.remove("colorR");
+  }
 });
 
-// div.addEventListener("contextmenu", (e) => {
-//   alert("HOLA GUAPO");
-// });
-// boton.addEventListener("dblclick", (e) => {
-//   alert("HOla Guapo")
-// });
+const validar = () => {
+  let error = [];
+  if (nombre.value.length < 4 || nombre.value.length > 30) {
+    error[0] = true;
+    error[1] = "Nombre no valido";
+    return error;
+  } else if (materia.value.length < 5 || materia.value.length > 30) {
+    error[0] = true;
+    error[1] = "Materia invalida";
+    return error;
+  } else if (
+    correo.value.length < 5 ||
+    correo.value.length > 30 ||
+    correo.value.indexOf("@") == -1 ||
+    correo.value.indexOf(".") == -1
+  ) {
+    error[0] = true;
+    error[1] = "Correo invalido";
+    return error;
+  }
 
-// function saludar() {
-//   alert("HOLA GUAPO DE NUEVO");
-// }
+  error[0] = false;
+  return error;
+};
